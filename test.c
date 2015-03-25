eca9579940165dd37520696bb9a592bd7c228c20
Ajith <joseajith8@gmail.com>
2015-03-25 15:59:52 +0530

    malloc bug fix

--- test.c
+++ test.c
@@ -7,7 +7,7 @@ void main()
     char *test;
     test = (char *)malloc(10);
     *test++ = 'c';
-
+    //malloc bug fix
     printf("0x%x Address in test \n",test);
     printf("0x%x Address of test \n",&test);
     printf("%s Value in test\n",*test);
